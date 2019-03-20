0fbc633086fc04a2d1981980529859b708abdb14
eropsyren <eropsyren@gmail.com>
2019-03-20 20:06:00 +0100

    add st instruction tests

--- src/instr/ops/store/mod.rs
+++ src/instr/ops/store/mod.rs
@@ -10,3 +10,20 @@ pub fn st(instr: u16, mem: &mut Memory) {
 
     mem.write(pc + pc_offset, val);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::st;
+    use crate::mem::Memory;
+
+    #[test]
+    fn test_st() {
+        let mut mem = Memory::new();
+        let instr = 0b_0011_001_000000100;
+        mem.write_pc(1);
+        mem.write_reg(1, 11);
+        st(instr, &mut mem);
+
+        assert_eq!(11, mem.read(1 + 4));
+    }
+}

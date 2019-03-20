a56139b504a1c89b7f22560a8f9e9b1f7b9f88a6
eropsyren <eropsyren@gmail.com>
2019-03-20 18:29:10 +0100

    add ld instruction tests

--- src/instr/ops/load/mod.rs
+++ src/instr/ops/load/mod.rs
@@ -12,3 +12,22 @@ pub fn ld(instr: u16, mem: &mut Memory) {
 
     mem.update_flags(dr);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ld;
+    use crate::mem::Memory;
+    use crate::mem::{COND_NEG, COND_POS, COND_ZRO};
+
+    #[test]
+    fn test_ld() {
+        let mut mem = Memory::new();
+        let instr = 0b_0010_110_000000011;
+        mem.write(3, 125);
+        mem.write_pc(0);
+        ld(instr, &mut mem);
+
+        assert_eq!(125, mem.read_reg(6));
+        assert_eq!(COND_POS, mem.read_cond());
+    }
+}

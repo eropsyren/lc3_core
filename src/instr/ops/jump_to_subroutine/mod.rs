e97e4e3de5fea9c677ca8d944fba6f8b2fc98a65
eropsyren <eropsyren@gmail.com>
2019-03-20 18:13:41 +0100

    add jsr instruction tests

--- src/instr/ops/jump_to_subroutine/mod.rs
+++ src/instr/ops/jump_to_subroutine/mod.rs
@@ -20,3 +20,32 @@ pub fn jsr(instr: u16, mem: &mut Memory) {
         mem.write_pc(val);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::jsr;
+    use crate::mem::Memory;
+
+    #[test]
+    fn test_jsr() {
+        let mut mem = Memory::new();
+        let instr = 0b_0100_1_00000000011;
+        mem.write_pc(255);
+        jsr(instr, &mut mem);
+
+        assert_eq!(255, mem.read_reg(7));
+        assert_eq!(255 + 3, mem.read_pc());
+    }
+
+    #[test]
+    fn test_jsrr() {
+        let mut mem = Memory::new();
+        let instr = 0b_0100_0_00_001_000000;
+        mem.write_reg(1, 4000);
+        mem.write_pc(255);
+        jsr(instr, &mut mem);
+
+        assert_eq!(255, mem.read_reg(7));
+        assert_eq!(4000, mem.read_pc());
+    }
+}

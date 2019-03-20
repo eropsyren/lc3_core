53b4ba43d277e536385002febfdf8e80c378fb3e
eropsyren <eropsyren@gmail.com>
2019-03-20 19:44:23 +0100

    add ldi instruction tests

--- src/instr/ops/load_indirect/mod.rs
+++ src/instr/ops/load_indirect/mod.rs
@@ -13,3 +13,23 @@ pub fn ldi(instr: u16, mem: &mut Memory) {
 
     mem.update_flags(dr);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::ldi;
+    use crate::mem::Memory;
+    use crate::mem::{COND_NEG, COND_POS, COND_ZRO};
+
+    #[test]
+    fn test_ldi() {
+        let mut mem = Memory::new();
+        let instr = 0b_1010_010_000000011;
+        mem.write_pc(1);
+        mem.write(4, 100);
+        mem.write(100, 0b_1111_1111_1111_1111);
+        ldi(instr, &mut mem);
+
+        assert_eq!(-1, mem.read_reg(2) as i16);
+        assert_eq!(COND_NEG, mem.read_cond());
+    }
+}

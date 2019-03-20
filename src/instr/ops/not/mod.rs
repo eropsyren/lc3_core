9709dc5bbf4b719cea01dd722448ddfa2dc7d76e
eropsyren <eropsyren@gmail.com>
2019-03-20 19:57:41 +0100

    add not instruction tests

--- src/instr/ops/not/mod.rs
+++ src/instr/ops/not/mod.rs
@@ -10,3 +10,21 @@ pub fn not(instr: u16, mem: &mut Memory) {
 
     mem.update_flags(dr);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::not;
+    use crate::mem::Memory;
+    use crate::mem::{COND_NEG, COND_POS, COND_ZRO};
+
+    #[test]
+    fn test_not() {
+        let mut mem = Memory::new();
+        let instr = 0b_1001_001_010_1_11111;
+        mem.write_reg(2, 0b_0000_1111_0000_1111);
+        not(instr, &mut mem);
+
+        assert_eq!(0b_1111_0000_1111_0000, mem.read_reg(1));
+        assert_eq!(COND_NEG, mem.read_cond());
+    }
+}

fa96047b6b6a2f4768d3f6a9954b779389ad3ac1
eropsyren <eropsyren@gmail.com>
2019-04-03 19:20:25 +0200

    rework MockIODevice Msg structs and add Val struct

--- src/io/mock.rs
+++ src/io/mock.rs
@@ -3,38 +3,51 @@ use super::IODevice;
 const a_char: u16 = 97;
 
 pub struct MockIODevice {
-    msg: Msg,
+    msg: Option<Msg>,
+    val: Option<Val>,
 }
 
 impl MockIODevice {
     pub fn new() -> MockIODevice {
-        MockIODevice { msg: Msg::Empty }
+        MockIODevice {
+            msg: None,
+            val: None,
+        }
     }
 
-    pub fn getMsg(&self) -> &Msg {
+    pub fn getMsg(&self) -> &Option<Msg> {
         &self.msg
     }
 }
 
 impl IODevice for MockIODevice {
     fn print_str(&mut self, str: &str) {
-        self.msg = Msg::PrintStr(String::from(str));
+        self.msg = Some(Msg::PrintStr);
+        self.val = Some(Val::String(String::from(str)));
     }
 
     fn print_char(&mut self, c: u16) {
-        self.msg = Msg::PrintChar(c);
+        self.msg = Some(Msg::PrintChar);
+        self.val = Some(Val::U16(c));
     }
 
     fn get_char(&mut self) -> u16 {
-        self.msg = Msg::GetChar(a_char);
+        self.msg = Some(Msg::GetChar);
+        self.val = Some(Val::U16(a_char));
 
         a_char
     }
 }
 
 pub enum Msg {
-    PrintStr(String),
-    PrintChar(u16),
-    GetChar(u16),
+    PrintStr,
+    PrintChar,
+    GetChar,
+    Empty,
+}
+
+pub enum Val {
+    String(String),
+    U16(u16),
     Empty,
 }

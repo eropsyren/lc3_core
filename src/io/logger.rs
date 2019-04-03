9f2335fa90d0daaeaeee3a5e51001bafcc783241
eropsyren <eropsyren@gmail.com>
2019-04-03 18:23:05 +0200

    add logger io device

---
+++ src/io/logger.rs
@@ -0,0 +1,29 @@
+use super::IODevice;
+
+const a_char: u16 = 97;
+
+pub struct IODeviceLogger {
+    msg: Msg,
+}
+
+impl IODevice for IODeviceLogger {
+    fn print_str(&mut self, str: &str) {
+        self.msg = Msg::PrintStr(String::from(str));
+    }
+
+    fn print_char(&mut self, c: u16) {
+        self.msg = Msg::PrintChar(c);
+    }
+
+    fn get_char(&mut self) -> u16 {
+        self.msg = Msg::GetChar(a_char);
+
+        a_char
+    }
+}
+
+enum Msg {
+    PrintStr(String),
+    PrintChar(u16),
+    GetChar(u16),
+}

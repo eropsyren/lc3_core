79e1b4927c0de177899df7691620ced60b00b12c
eropsyren <eropsyren@gmail.com>
2019-04-04 20:03:32 +0200

    add halt trap test

--- src/instr/ops/trap/traps/halt/mod.rs
+++ src/instr/ops/trap/traps/halt/mod.rs
@@ -4,3 +4,20 @@ use crate::mem::Memory;
 pub fn halt(mem: &mut Memory, controller: &mut Controller) {
     controller.stop();
 }
+
+#[cfg(test)]
+mod tests {
+    use super::halt;
+    use crate::controller::Controller;
+    use crate::mem::Memory;
+
+    #[test]
+    fn test_halt() {
+        let mut mem = Memory::new();
+        let mut controller = Controller::new();
+
+        halt(&mut mem, &mut controller);
+
+        assert_eq!(false, controller.is_running());
+    }
+}

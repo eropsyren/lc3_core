748415ece06f6fb499321f71b8dd242e3235e508
eropsyren <eropsyren@gmail.com>
2019-04-04 19:58:38 +0200

    add getc trap test

--- src/instr/ops/trap/traps/getc/mod.rs
+++ src/instr/ops/trap/traps/getc/mod.rs
@@ -4,3 +4,26 @@ use crate::mem::Memory;
 pub fn getc(mem: &mut Memory, io: &mut dyn IODevice) {
     mem.write_reg(0, io.get_char());
 }
+
+#[cfg(test)]
+mod tests {
+    use super::getc;
+    use crate::io::MockIODevice;
+    use crate::io::Val;
+    use crate::mem::Memory;
+
+    #[test]
+    fn test_getc() {
+        let mut mem = Memory::new();
+        let mut io = MockIODevice::new();
+
+        getc(&mut mem, &mut io);
+
+        let val = match io.get_val() {
+            Some(Val::U16(c)) => *c,
+            _ => unreachable!(),
+        };
+
+        assert_eq!(val, mem.read_reg(0));
+    }
+}

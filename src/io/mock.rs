68f7df8d092c81e0e453a2817f7484860532cf67
eropsyren <eropsyren@gmail.com>
2019-04-05 18:23:52 +0200

    rework MockIODevice

--- src/io/mock.rs
+++ src/io/mock.rs
@@ -3,54 +3,53 @@ use super::IODevice;
 const a_char: u16 = 97;
 
 pub struct MockIODevice {
-    msg: Option<Msg>,
-    val: Option<Val>,
+    logs: Vec<(Msg, Val)>,
 }
 
 impl MockIODevice {
     pub fn new() -> MockIODevice {
-        MockIODevice {
-            msg: None,
-            val: None,
+        MockIODevice { logs: vec![] }
+    }
+
+    pub fn logs(&self) -> &Vec<(Msg, Val)> {
+        &self.logs
+    }
+
+    pub fn last(&self) -> Option<(Msg, Val)> {
+        let last = self.logs.last();
+
+        match last {
+            Some((m, v)) => Some((m.clone(), v.clone())),
+            None => None,
         }
-    }
-
-    pub fn get_msg(&self) -> &Option<Msg> {
-        &self.msg
-    }
-
-    pub fn get_val(&self) -> &Option<Val> {
-        &self.val
     }
 }
 
 impl IODevice for MockIODevice {
     fn print_str(&mut self, str: &str) {
-        self.msg = Some(Msg::PrintStr);
-        self.val = Some(Val::String(String::from(str)));
+        self.logs
+            .push((Msg::PrintStr, Val::String(String::from(str))));
     }
 
     fn print_char(&mut self, c: u16) {
-        self.msg = Some(Msg::PrintChar);
-        self.val = Some(Val::U16(c));
+        self.logs.push((Msg::PrintChar, Val::U16(c)));
     }
 
     fn get_char(&mut self) -> u16 {
-        self.msg = Some(Msg::GetChar);
-        self.val = Some(Val::U16(a_char));
+        self.logs.push((Msg::GetChar, Val::U16(a_char)));
 
         a_char
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Msg {
     PrintStr,
     PrintChar,
     GetChar,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 pub enum Val {
     String(String),
     U16(u16),
@@ -58,6 +57,7 @@ pub enum Val {
 
 #[cfg(test)]
 mod tests {
+    use super::a_char;
     use super::MockIODevice;
     use super::Msg;
     use super::Val;
@@ -66,25 +66,43 @@ mod tests {
     #[test]
     fn test_new() {
         let io = MockIODevice::new();
+        let v: Vec<(Msg, Val)> = vec![];
 
-        assert_eq!(io.msg, None);
-        assert_eq!(io.val, None);
+        assert_eq!(v, io.logs);
     }
 
     #[test]
-    fn test_get_msg() {
-        let mut io = MockIODevice::new();
-        io.msg = Some(Msg::GetChar);
+    fn test_logs() {
+        let io = MockIODevice::new();
+        let v: Vec<(Msg, Val)> = vec![
+            (Msg::PrintStr, Val::String(String::from("s"))),
+            (Msg::PrintChar, Val::U16(0)),
+            (Msg::GetChar, Val::U16(a_char)),
+        ];
 
-        assert_eq!(Some(Msg::GetChar), *io.get_msg());
+        io.print_str("s");
+        io.print_char(0);
+        io.get_char();
+
+        for (expected, found) in v.iter().zip(io.logs().iter()) {
+            assert_eq!(expected, found);
+        }
     }
 
     #[test]
-    fn test_get_val() {
-        let mut io = MockIODevice::new();
-        io.val = Some(Val::U16(11));
+    fn last() {
+        let io = MockIODevice::new();
+        let v: Vec<(Msg, Val)> = vec![
+            (Msg::PrintStr, Val::String(String::from("s"))),
+            (Msg::PrintChar, Val::U16(0)),
+            (Msg::GetChar, Val::U16(a_char)),
+        ];
 
-        assert_eq!(Some(Val::U16(11)), *io.get_val());
+        io.print_str("s");
+        io.print_char(0);
+        io.get_char();
+
+        assert_eq!(Some((Msg::GetChar, Val::U16(a_char))), io.last());
     }
 
     #[test]
@@ -92,10 +110,9 @@ mod tests {
         let mut io = MockIODevice::new();
         io.print_str("This is a &str");
 
-        assert_eq!(Some(Msg::PrintStr), *io.get_msg());
         assert_eq!(
-            Some(Val::String(String::from("This is a &str"))),
-            *io.get_val()
+            Some((Msg::PrintStr, Val::String(String::from("This is a &str")))),
+            io.last()
         );
     }
 
@@ -104,8 +121,7 @@ mod tests {
         let mut io = MockIODevice::new();
         io.print_char(11);
 
-        assert_eq!(Some(Msg::PrintChar), *io.get_msg());
-        assert_eq!(Some(Val::U16(11)), *io.get_val());
+        assert_eq!(Some((Msg::PrintChar, Val::U16(11))), io.last());
     }
 
     #[test]
@@ -113,6 +129,6 @@ mod tests {
         let mut io = MockIODevice::new();
         io.get_char();
 
-        assert_eq!(Some(Msg::GetChar), *io.get_msg());
+        assert_eq!(Some((Msg::GetChar, Val::U16(a_char))), io.last());
     }
 }

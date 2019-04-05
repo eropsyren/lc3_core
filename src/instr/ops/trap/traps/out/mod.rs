76f5cfae5eb19bad5368fb986e58fb0f28fcb577
eropsyren <eropsyren@gmail.com>
2019-04-05 19:27:15 +0200

    add out trap test

--- src/instr/ops/trap/traps/out/mod.rs
+++ src/instr/ops/trap/traps/out/mod.rs
@@ -6,3 +6,28 @@ pub fn out(mem: &mut Memory, io: &mut dyn IODevice) {
 
     io.print_char(c);
 }
+
+#[cfg(test)]
+mod tests {
+    use super::out;
+    use crate::io::IODevice;
+    use crate::io::MockIODevice;
+    use crate::io::Msg;
+    use crate::io::Val;
+    use crate::mem::Memory;
+
+    #[test]
+    fn test_out() {
+        let mut mem = Memory::new();
+        let mut io = MockIODevice::new();
+
+        out(&mut mem, &mut io);
+
+        let c = match io.last() {
+            Some((Msg::PrintChar, Val::U16(c))) => c,
+            _ => unreachable!(),
+        };
+
+        assert_eq!(mem.read_reg(0), c);
+    }
+}
